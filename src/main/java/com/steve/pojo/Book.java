6ab3596d8f0e5192d5b6cd0f7e59ed300882c522
Steve-Kung <stevekung98@gmail.com>
2020-01-03 19:15:21 +0800

    200103ssmbulid

---
+++ src/main/java/com/steve/pojo/Book.java
@@ -0,0 +1,15 @@
+package com.steve.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Book {
+    private int bookId;
+    private String bookName;
+    private int bookCounts;
+    private String bookDetails;
+}
